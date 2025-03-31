02fef19b8909ca71d07fb82edadc98539f54de9e
Felipe B <165314811+felpb2@users.noreply.github.com>
2025-03-31 10:08:37 -0300

    Ferozes e curiosos (n)

--- main.cpp
+++ main.cpp
@@ -1,114 +1,64 @@
-#include<bits/stdc++.h>
-
-#define mp make_pair
-#define f first
-#define s second
-#define mt make_tuple
-const int INF = 1e9;
-
-using namespace std;
-
-struct retangulo{
-	pair<double,double> Ecima,Ebaixo,Dcima,Dbaixo;
-};
-
-bool maior_cb( retangulo &p1, retangulo &ret ){
-	return p1.Ebaixo.f <= ret.Ecima.f && p1.Dbaixo.f >= ret.Dcima.f;
+#include <bits/stdc++.h>
+ using namespace std;
+ 
+ #define _ cin.tie(0);ios::sync_with_stdio(0);
+ #define mp make_pair
+ #define mt make_tuple
+ #define f first
+ #define s second
+ 
+bool prod( pair<double,double> p1, pair<double, double> p2 ){
+	double produto = p1.f * p2.s - p1.s * p2.f;
+	return (produto >= 0 ? true : false );  
 }
 
-bool maior_lo( retangulo &p1, retangulo &ret ){
-	return p1.Ecima.s >= ret.Ecima.s && p1.Ebaixo.s <= ret.Ebaixo.s;
-}
-
-double distpp(pair<double,double> p1, pair<double,double> p2){
-	return sqrt(pow(p2.f-p1.f,2)+pow(p2.s-p1.s,2));
-}
-
-bool lado(retangulo &p1, retangulo &ret, int AAH){
-	double temp = 0;	
-	if( p1.Ebaixo.s > ret.Ecima.s ){
-		if( (p1.Dcima.f <= ret.Dcima.f && p1.Ecima.f >= ret.Ecima.f || p1.Ecima.f <= ret.Dcima.f && p1.Dcima.f > ret.Dcima.f || p1.Ecima.f < ret.Ecima.f && p1.Dcima.f >= ret.Ecima.f) || maior_cb(p1,ret) ){
-			
-			temp = p1.Ebaixo.s - ret.Ecima.s;
-
-		}else{
-			//distpp
-			if( p1.Dcima.f < ret.Ecima.f){ temp = distpp(p1.Dbaixo, ret.Ecima); }
-			else{ temp = distpp(p1.Ebaixo, ret.Dcima);}
-		}
-	}else if( p1.Ecima.s < ret.Ebaixo.s){
-
-		if( (p1.Dcima.f <= ret.Dcima.f && p1.Ecima.f >= ret.Ecima.f || p1.Ecima.f <= ret.Dcima.f && p1.Dcima.f > ret.Dcima.f || p1.Ecima.f < ret.Ecima.f && p1.Dcima.f >= ret.Ecima.f) || maior_cb(p1,ret) ){
-			
-			temp = ret.Ebaixo.s - p1.Ecima.s;
-
-		}else{
-			//distpp
-			if( p1.Dcima.f < ret.Dbaixo.f){ temp = distpp(p1.Dcima, ret.Ebaixo);}
-			else{ temp = distpp(p1.Ecima, ret.Dbaixo);}
-		}
-	}else if( p1.Ecima.f > ret.Dcima.f){
-		if( (p1.Ebaixo.s >= ret.Dbaixo.s && p1.Ecima.s <= ret.Dcima.s || p1.Ebaixo.s <= ret.Dcima.s && p1.Ecima.s > ret.Dcima.s || p1.Ecima.s >= ret.Dbaixo.s && p1.Ebaixo.s < ret.Dbaixo.s) || maior_lo(p1,ret)){
-			
-			temp = p1.Ecima.f - ret.Dcima.f;
-
-		}else{
-			//distpp
-			if( p1.Dbaixo.s > ret.Ecima.s ){ temp = distpp(p1.Ebaixo, ret.Dcima ); }
-			else{ temp = distpp(p1.Ecima, ret.Dbaixo); }
-		}
-	}else if( p1.Dcima.f < ret.Ecima.f){
+int main() {
+ 	_	
+ 	
+ 	int pretos,brancos;
+ 	while(cin >> pretos >> brancos){
+ 
+ 		vector<pair<double,double>> pret,branc;
+  
+ 		for(int i=0; i < pretos; i++){
+ 			double x,y;
+ 			cin >> x >> y;
+     		pret.push_back(mp(x,y));			
+ 		}
+ 	
+ 		for(int i=0; i < brancos; i++){
+ 			double x,y;
+ 			cin >> x >> y;
+ 			branc.push_back(mp(x,y));
+ 		}
+        
+ 
+ 		int contador;
+		double energia = 0;
+ 		pair<double,double> ij, jc, ci, ip, jp, cp;
 		
-		if( (p1.Ebaixo.s >= ret.Dbaixo.s && p1.Ecima.s <= ret.Dcima.s || p1.Ebaixo.s <= ret.Dcima.s && p1.Ecima.s > ret.Dcima.s || p1.Ecima.s >= ret.Dbaixo.s && p1.Ebaixo.s < ret.Dbaixo.s) || maior_lo(p1,ret)){
-			
-			temp = ret.Ecima.f - p1.Dcima.f;
-
-		}else{
-			//distpp
-			if( p1.Dbaixo.s > ret.Ecima.s ){ temp = distpp( p1.Dbaixo, ret.Ecima );}
-			else{ temp = distpp( p1.Dcima, ret.Ebaixo);}
-		}
-	}
-	return ( temp <= AAH ? true : false );
-}
-
-bool veri(retangulo &ret, retangulo &p2){
-	
-}
-
-int main(){
-	
-	cin.tie(0); ios::sync_with_stdio(0);
-	
-	int n, AAH;
-	cin >> n >> AAH;
-	
-	retangulo andares[n];
-
-	for(int i=0; i < n; i++){
-		double xc,yc,lar;
-		cin >> xc >> yc >> lar;
-		andares[i].Ecima = mp(xc-lar/2,yc+lar/2);
-		andares[i].Ebaixo = mp(xc-lar/2,yc-lar/2);
-		andares[i].Dcima = mp(xc+lar/2,yc+lar/2);
-		andares[i].Dbaixo = mp(xc+lar/2,yc-lar/2);
-	}
-
-	bool fail = false;
-	for(int i=0; i < n-1; i++){
-		if( !veri(andares[i],andares[i+1])){
-			if( !lado( andares[i], andares[i+1], AAH ) ){ 
-				fail = true;
-				break; 
-			}
-		}
-	}
-	
-	if( fail ){
-		cout << "OUCH\n";	
-	}else{
-		cout << "YEAH\n";
-	}
-	
-	return 0;	
-}
+		for(int i = 0; i < pret.size() - 2; i++){
+ 			for(int j = i + 1; j < pret.size() - 1; j++){
+ 				for(int c = j + 1; c < pret.size(); c++){
+					contador = 0;
+					ij = mp( pret[j].f - pret[i].f, pret[j].s - pret[i].s);
+					jc = mp( pret[c].f - pret[j].f, pret[c].s - pret[j].s);
+					ci = mp( pret[i].f - pret[c].f, pret[i].s - pret[c].s);
+ 					for(int v=0;  v < branc.size(); v++){
+						ip = mp( branc[v].f - pret[i].f, branc[v].s - pret[i].s);
+						jp = mp( branc[v].f - pret[j].f, branc[v].s - pret[j].s);
+						cp = mp( branc[v].f - pret[c].f, branc[v].s - pret[c].s);
+ 						if( prod(ij, ip) && prod(jc, jp) && prod(ci, cp) ){
+							contador++;
+						}
+ 					}
+ 					energia += pow(contador,2);
+ 				}
+ 			}
+ 		}
+ 		
+ 		cout << energia << '\n';
+ 	}
+ 	
+     return 0;
+ }
